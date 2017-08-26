83a05427aa8d827c3a098f5477ee7125b0c802bb
Sebuliba-Adrian <adrian.sebuliba1@gmail.com>
2017-08-26 10:37:35 +0300

    Add files via upload

---
+++ models.py
@@ -0,0 +1,11 @@
+import re
+from marshmallow import Schema, fields, pre_load
+from marshmallow import validate
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+from passlib.apps import custom_app_context as password_context
+
+
+
+db = SQLAlchemy()
+ma = Marshmallow()
---
+++ views.py
@@ -0,0 +1,3 @@
+from flask import Blueprint, request, jsonify, make_response
+
+api_bp = Blueprint('api', __name__)
